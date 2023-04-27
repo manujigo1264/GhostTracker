7e585234749cb9e593747edc2a3b6cd1ea5246f8
0xtron <emmanuel.jigo29@gmail.com>
2023-04-27 11:34:06 -0400

    Modified to include more details in IP and phone numbers

--- ghosttrack.py
+++ ghosttrack.py
@@ -1,83 +1,75 @@
-#!/usr/bin/env python3
-# Improved version of the original script
-
 import json
-import requests
-import time
 import os
-import phonenumbers
-from phonenumbers import carrier, geocoder, timezone
-from sys import stderr
+import requests
 import sys
+import time
+import phonenumbers
+from phonenumbers import carrier, geocoder, timezone, number_type
 
-# Separate functions for each functionality
 def print_banner():
-    stderr.writelines("""
-       ________               __      ______                __  
-      / ____/ /_  ____  _____/ /_    /_  __/________ ______/ /__
-     / / __/ __ \/ __ \/ ___/ __/_____/ / / ___/ __ `/ ___/ //_/
-    / /_/ / / / / /_/ (__  ) /_/_____/ / / /  / /_/ / /__/ ,<   
-    \____/_/ /_/\____/____/\__/     /_/ /_/   \__,_/\___/_/|_|  
-
-              [ + ]  Refactored by 0xtron  [ + ]  
-
-        [ 1 ] IP Tracker
-
-        [ 2 ] Phone Tracker
-
-        [ 0 ] Exit
+    sys.stderr.write("""
+    ***********************************************
+    *          IP and Phone Number Tracker        *
+    *               @WRITTEN BY 0XTRON            *
+    ***********************************************
     """)
 
 def track_ip(ip):
-    # Retrieve and process IP information
     try:
-        req_api = requests.get(f"http://ipwho.is/{ip}")
+        req_api = requests.get(f"http://ip-api.com/json/{ip}")
         ip_data = json.loads(req_api.text)
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
         return
 
-    print(f'============= SHOW INFORMATION IP ADDRESS =============')
-    # Print the results
-    print(f"\n IP target       :", ip)
-    # ... Additional lines for printing IP information ...
-    
-def track_phone(phone_number):
-    default_region = "ID"
-
-    try:
-        parsed_number = phonenumbers.parse(phone_number, default_region)
-    except phonenumbers.NumberParseException as e:
-        print(f"Error: {e}")
+    if ip_data['status'] == 'fail':
+        print("Error: IP not found")
         return
 
-    print(f"\n ========== SHOW INFORMATION PHONE NUMBERS ==========")
-    # Print the results
-    # ... Additional lines for printing phone number information ...
+    print(f'\n============= DETAILED INFORMATION IP ADDRESS =============')
+    for key, value in ip_data.items():
+        print(f" {key.capitalize(): <16}: {value}")
+
+def track_phone_number(phone_number):
+    default_region = "ID"
+    parsed_number = phonenumbers.parse(phone_number, default_region)
+    region_code = phonenumbers.region_code_for_number(parsed_number)
+    operator = carrier.name_for_number(parsed_number, "en")
+    location = geocoder.description_for_number(parsed_number, "id")
+    time_zones = timezone.time_zones_for_number(parsed_number)
+    number_type_description = phonenumbers.number_type(parsed_number)
+
+    print(f"\n========== DETAILED INFORMATION PHONE NUMBERS ==========")
+    print(f"Phone Number        : {phonenumbers.format_number(parsed_number, phonenumbers.PhoneNumberFormat.E164)}")
+    print(f"Location            : {location}")
+    print(f"Region Code         : {region_code}")
+    print(f"Operator            : {operator}")
+    print(f"Number Type         : {number_type_description}")
+    print(f"Time Zones          : {', '.join(time_zones)}")
+    print(f"Valid               : {phonenumbers.is_valid_number(parsed_number)}")
+    print(f"Possible            : {phonenumbers.is_possible_number(parsed_number)}")
 
 def main():
+    print_banner()
+
     while True:
-        os.system('clear')
-        print_banner()
-        input_user = input('\n   @Ghost~# ')
+        print("\n[1] IP Tracker")
+        print("[2] Phone Tracker")
+        print("[0] Exit")
 
-        if input_user == '1':
-            os.system('clear')
-            ip = input("\n Enter IP target : ")
+        choice = input("\nPlease choose an option: ")
+
+        if choice == "1":
+            ip = input("\nEnter IP target: ")
             track_ip(ip)
-
-        elif input_user == '2':
-            os.system('clear')
-            phone_number = input("\n Enter phone number target Ex [+6281xxxxxxxxx] : ")
-            track_phone(phone_number)
-
-        elif input_user == '0':
-            print("\n  [!] THANK'S FOR USING TOOL GHOST-TRACK !")
+        elif choice == "2":
+            phone_number = input("\nEnter phone number target (Ex: +6281xxxxxxxxx): ")
+            track_phone_number(phone_number)
+        elif choice == "0":
+            print("Exiting...")
             break
-
         else:
-            print(" Opss no option !")
-            time.sleep(2)
+            print("Invalid option. Please try again.")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
